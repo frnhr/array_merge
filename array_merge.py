2a403838f8cdf078726cdadcbb62a4a955e1addb
frnhr <fran.hrzenjak@gmail.com>
2016-03-21 19:12:43 +0100

    lists_merge, any number of lists

--- array_merge.py
+++ array_merge.py
@@ -1,9 +1,7 @@
 
-def list_merge(list1, list2):
-    """ Merge two sorted lists.
-    :param list1: list, sorted
-    :param list2: list, sorted
-    :return: merged list, sorted
+def list_merge(*lists):
+    """ Merge N sorted lists.
+    :return: merged lists, sorted
 
     Tests:
     >>> l1 = [2, 3, 5, 6, 8, 9, 9, 9, 10, 11, 13, 15, ]
@@ -16,27 +14,46 @@ def list_merge(list1, list2):
     [1]
     >>> list_merge([1], [1])
     [1, 1]
+    >>> list_merge([], [], [], [])
+    []
+    >>> list_merge([], [2], [4], [])
+    [2, 4]
+    >>> list_merge([], [], [], [0])
+    [0]
+    >>> import random, time
+    >>> huge1 = sorted([random.randrange(100000000) for _ in range(1000000)])
+    >>> huge2 = sorted([random.randrange(100000000) for _ in range(1000000)])
+    >>> huge3 = sorted([random.randrange(100000000) for _ in range(1000000)])
+    >>> t0 = time.time()
+    >>> result = list_merge(huge1, huge2, huge3)
+    >>> t1 = time.time()
+    >>> print("Run time for list_merge: {}".format(t1 - t0))  #doctest:+ELLIPSIS
+    Run time for list_merge: ...
+    >>> for i in range(len(result) - 1):
+    ...     assert result[i] <= result[i + 1], "Items: {}, {}".format(result[i], result[i+1])
     """
-    counters = [0, 0]
-    lists = (list1, list2)
+    counters = [0] * len(lists)
     result = []
-    total = len(list1) + len(list2)
-    while sum(counters) < total:
-        if counters[0] >= len(lists[0]):
-            # nothing left in list 0, use 1:
-            next_from = 1
-        elif counters[1] >= len(lists[1]):
-            # nothing left in list 1, use 0:
-            next_from = 0
-        else:
-            # both lists have more elements, pick minimum:
-            if lists[0][counters[0]] <= lists[1][counters[1]]:
-                next_from = 0
+    next_from = True
+    while next_from is not None:
+        next_from = None
+        for list_i, counter in enumerate(counters):
+            if counter is None:
+                continue
+            if counter >= len(lists[list_i]):
+                counters[list_i] = None
+                continue
+            if next_from is None:
+                # first non-eshausted list, take the element:
+                next_from = list_i
             else:
-                next_from = 1
-        # append minimum to result:
-        result.append(lists[next_from][counters[next_from]])
-        # increase counter:
-        counters[next_from] += 1
-        # all done:
+                # compare and take if smaller:
+                if lists[list_i][counters[list_i]] < lists[next_from][counters[next_from]]:
+                    next_from = list_i
+        if next_from is not None:
+            # append minimum to result:
+            result.append(lists[next_from][counters[next_from]])
+            # increase counter:
+            counters[next_from] += 1
+            # all done:
     return result
